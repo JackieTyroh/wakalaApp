3da0c23d8f186dc12040aa14223ff15fa7ac5a7e
JackieTyroh <51116510+JackieTyroh@users.noreply.github.com>
2020-07-24 21:19:57 +0300

    Add files via upload

---
+++ lib/homepage.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+
+
+class MyHomePage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WAKALA'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20,),
+        child:ListView(
+            children: <Widget>[
+              MaterialButton(
+                onPressed: (){},
+                height: 60,
+                minWidth: 150,
+                child: Image.asset('assets/tigopesa.png'),
+              ),
+              SizedBox(height: 100,),
+              MaterialButton(
+                onPressed: (){},
+                height: 60,
+                minWidth: 150,
+                child: Image.asset('assets/mpesa.jpeg'),
+              ),
+              SizedBox(height: 100,),
+              MaterialButton(
+                onPressed: (){},
+                height: 60,
+                minWidth: 150,
+                child: Image.asset('assets/airtel.png'),
+              ),
+              SizedBox(height: 100,),
+              MaterialButton(
+                onPressed: (){},
+                height: 60,
+                minWidth: 150,
+                child: Image.asset('assets/halopesa.png'),
+              ),
+              SizedBox(height: 100,),
+              MaterialButton(onPressed: (){},
+              height: 60,
+              minWidth: 150,
+              child: Image.asset('assets/tpesa.png'),
+              ),
+              SizedBox(height: 100,),
+            ],
+          ),
+      ),
+      
+    );
+  }
+}
